ec8e0b30ccbfc5a645a8a5aac05a9eadb498113f
katiii-git <katymartagon@gmail.com>
2025-03-06 11:37:23 -0600

    Vincular firebase y consumo del service header & work-experience

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import {SkillsService} from '../services/skills-service/skills.service';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
-
+constructor (public skillsService: SkillsService){
+console.log(this.skillsService);
+}
 }
