7331aba01c67bf3be218fca57b52d959ac6c5a0a
katiii-git <katymartagon@gmail.com>
2025-03-14 11:01:40 -0600

    Inyectando Education a mycv #2

---
+++ src/app/models/education/education.model.ts
@@ -0,0 +1,8 @@
+export class Education{
+	id?: string;
+	fechaInic?: string='AAAA';
+	fechaFin?: string='AAAA';
+	titulo?: string='titulo';
+	escuela?: string='nombre';
+
+}
--- src/app/services/education-service/education.service.ts
+++ src/app/services/education-service/education.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Education } from '../../models/education/education.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EducationService {
 
-  constructor() { }
+  private dbPath='/education';
+
+        educationRef: AngularFirestoreCollection<Education>;
+
+  constructor(private db: AngularFirestore) {
+        this.educationRef=db.collection(this.dbPath);
+  }
+
+  getEducation(): AngularFirestoreCollection<Education>{
+    return this.educationRef;
+  }
 }
+
+
